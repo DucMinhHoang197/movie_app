3e47d7d39ac549ba960fa9367aa34ebc75ca30ad
DucMinhHoang197 <ducminhhoang197@gmail.com>
2024-04-05 12:39:42 +0700

    Update MainLayout.js

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
-import { Stack } from "@mui/material";
-// import MainFooter from "./MainFooter";
+
 import Header from "../layouts/Header";
 function MainLayout() {
   return (
